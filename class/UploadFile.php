959aeeef71b30f4c862bf838cbc5521a676b0935
Milan Petrovic <micko.kv@gmail.com>
2014-04-01 04:47:43 +0200

    Add method for displaying different error messages

--- class/UploadFile.php
+++ class/UploadFile.php
@@ -13,9 +13,16 @@ class UploadFile
 {
 
     protected $destination;
+    protected $messages = array();
 
 
-
+    /**
+     * Takes path to upload directory, checks if it's writable and valid.
+     * If trailing slash is omitted, adds it.
+     *
+     * @param $uploadDir String
+     * @throws \Exception
+     */
     public function __construct($uploadDir)
     {
         if ( !is_dir($uploadDir) || !is_writable($uploadDir) ) {
@@ -28,4 +35,52 @@ class UploadFile
 
         $this->destination = $uploadDir;
     }
+
+    public function upload()
+    {
+        $uploadedFile = current($_FILES);
+        if ( $this->checkFile($uploadedFile) ) {
+            $this->moveFile($uploadedFile);
+        }
+    }
+
+    /**
+     * Takes reference to current file in $_FILES array as argument...
+     *
+     * @param $file Array
+     * @return bool
+     */
+    protected function checkFile($file)
+    {
+        if ( $file["error"] != 0 ) {
+            $this->getErrorMessage($file);
+            return false;
+        }
+        return true;
+    }
+
+    protected function getErrorMessage($file)
+    {
+        switch( $file["error"] ) {
+            case 1:
+            case 2:
+                $this->messages[] = $file["name"] . " is too big to upload.";
+                break;
+            case 3:
+                $this->messages[] = $file["name"] . " was only partially uploaded.";
+                break;
+            case 4:
+                $this->messages[] = "No file selected";
+                break;
+            default:
+                $this->messages[] = "There was a problem uploading " . $file["name"];
+                break;
+        }
+    }
+
+    protected function moveFile($file)
+    {
+        echo $file['name'] . " was uploaded successfully";
+    }
+
 } 
